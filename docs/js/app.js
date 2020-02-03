ea8a89895d1ccd29d975f4b431071b60fb87fdc2
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-02-03 22:27:11 +0100

    update head

---
+++ docs/js/app.js
@@ -0,0 +1,11 @@
+fetch('https://swapi.co/api/people/1')
+  .then((response) => {
+    return response.json();
+    
+  })
+  .then((myJson) => {
+    console.log(myJson)
+    // Declare variables
+    });
+
+
