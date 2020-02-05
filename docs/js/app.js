016a0d20e90aa555ead3b217abb22afa5120a035
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-02-05 11:22:42 +0100

    Update

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,63 +1,83 @@
-let button = document.querySelector('#button')
-let selection = document.getElementById("categorySelect")
-let collection = document.getElementById("collection")
+let button = document.querySelector("#button");
+let selection = document.getElementById("categorySelect");
+let collection = document.getElementById("collection");
+
 
 // fetch data
-fetch('https://swapi.co/api/')
-  .then((response) => {
-
+fetch("https://swapi.co/api/")
+  .then(response => {
     return response.json();
-    
   })
-  .then((myJson) => {
-    console.log(myJson)
+  .then(myJson => {
+    console.log(myJson);
     // Declare variables
-    let categories = Object.keys(myJson)
+    let categories = Object.keys(myJson);
     categories.forEach(element => {
-      selection.insertAdjacentHTML('beforeend', `<option value="${element}"> ${element}</option>`)
+      selection.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${element}"> ${element}</option>`
+      );
     });
-
-    })
-    .catch((err) => {
-      console.log(err)
-    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
 // Retrieve data from selected value
-selection.onchange = getData, removeData;
-function removeData(){
-  
+(selection.onchange = removeData);
+function removeData() {
+  let myNode = document.getElementById("collection");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+
+  }
+  return getData(this)
+
 }
-function getData(){
-  var value = this.value
+function getData(parameter) {
+  // collection.innerHTML = "";
+  var value = parameter.value;
   console.log(value);
-  
-fetch('https://swapi.co/api/' + value)
-.then((response) => {
 
-  return response.json();
-  
-})
-.then((myJson) => {
-  console.log(myJson)
-  // Declare variables
-  let items = myJson.results
-  console.log(items)
-  collection.remove
-  // newItems = items.filter(item => { console.log(item.key)})
-  // console.log(newItems, 'aaaaaaaaaa')
-  
-  items.forEach(element => {
-  
-    collection.insertAdjacentHTML('afterbegin', `<section id="${element}"></section>`)
-    let placeholder = document.getElementById(element)
+  fetch("https://swapi.co/api/" + value)
+    .then(response => {
+      return response.json();
+    })
+    .then(myJson => {
+      console.log(myJson);
+      // Declare variables
+      let items = myJson.results;
+      console.log(Object.entries(items));
+    
+      items.forEach(element => {
+        delete element.homeworld;
+        delete element.url;
+        delete element.people;
+        delete element.films;
+        delete element.created;
+        delete element.edited;
+        delete element.vehicles;
+        delete element.starships;
+        delete element.characters;
+        delete element.planets;
+        delete element.species;
+        delete element.pilots;
+        delete element.residents;
+        
+        collection.insertAdjacentHTML(
+          "afterbegin",
+          `<section id="${element}"></section>`
+        );
+        let placeholder = document.getElementById(element);
 
-    // collection.insertAdjacentHTML('beforeend',`<p>${JSON.stringify(element)}</p>`)
-    Object.entries(element).forEach(([key, value]) => {
-     if((key !== "homeworld") ||(key !== "people")||( key !==  "url")){
-      return placeholder.insertAdjacentHTML('beforeend', `<li>${key}: ${value}</li>`)
-    }})
-  });
-  });
-} 
+        Object.entries(element).forEach(([key, value]) => {
+          placeholder.insertAdjacentHTML(
+            "beforeend",
+            `<li>${key}: ${value}</li>`
+          );
+        });
+      });
+    });
+}
 
 // function makeElement(element){
 //   return document.createElement(element)
@@ -70,8 +90,6 @@ fetch('https://swapi.co/api/' + value)
 // function appendElemnten(element, parentelement){
 //   return parentelement.appendChild(element)
 // }
-
-
 
 // let ul = makeElement("ul")
 // let films = creatLi("li", item.films)
